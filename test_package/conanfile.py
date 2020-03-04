ebec3ea2bbe6dc4e19fefcc90882ab6d8db977a1
Silvio Kunaschk <silvio.kunaschk@gmail.com>
2020-03-05 00:12:05 +0100

    improved test package build with make

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,6 +1,7 @@
 import os
 
 from conans import ConanFile, CMake, AutoToolsBuildEnvironment, tools
+from conans.tools import os_info, SystemPackageTool
 
 
 class VCVRackSDKInstallerTestConan(ConanFile):
@@ -9,19 +10,26 @@ class VCVRackSDKInstallerTestConan(ConanFile):
 
     exports_sources = "*"
 
+    def system_requirements(self):
+        if self.settings.os == "Windows":
+            os.environ["PATH"] += os.pathsep + os.pathsep.join(self.env["PATH"]) + os.pathsep + "c:\\.conan\\80265f\\1\\bin\\mingw64\\bin"
+
     def build(self):
-        self.output.info("generate stuff with helper first?")
+        self.output.info("generate plugin src just with helper?")
         self._buildWithCMake()
         self._buildWithMake()
 
     def test(self):
-        self.output.info("Rack SDK test ...?")
+        self.output.info("nothing to run here")
 
     def _buildWithCMake(self):
+        self.output.info("building test plugin with CMake ...")
         cmake = CMake(self)
         cmake.configure(build_dir="cmake-build")
         cmake.build(build_dir="cmake-build")
 
     def _buildWithMake(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.make(target="-f ../../Makefile")
+        self.output.info("building test plugin with Make ...")
+        with tools.chdir("../.."):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.make()
